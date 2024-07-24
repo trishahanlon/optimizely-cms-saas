4f500e40f60aae6a03931fdee6dda282b1953487
Trisha Hanlon <tht8427@gmail.com>
2024-07-24 15:08:43 -0500

    Adding Optimizely One npm module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,15 @@ import { ThemeProvider, Body } from '@/components/theme'
 import { Figtree } from "next/font/google"
 import "./globals.scss"
 
+// Optimizely One
+import { Scripts } from '@remkoj/optimizely-one-nextjs/server'
+import { OptimizelyOneProvider, PageActivator } from '@remkoj/optimizely-one-nextjs/client'
+import OptimizelyOneGadget from '@remkoj/optimizely-one-nextjs/optimizely-one-gadget'
+
+export type RootLayoutProps = {
+    children: React.ReactNode
+}
+
 const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +34,9 @@ type RootLayoutProps = Readonly<PropsWithChildren<{}>>
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return <html lang="en">
+        <head>
+            <Scripts.Header />
+        </head>
         <ThemeProvider value={{ theme: "system" }}>
             <Body className={`${figtree.className} bg-ghost-white text-vulcan dark:bg-vulcan dark:text-ghost-white`}>
                 <div className="flex min-h-screen flex-col justify-between">
@@ -32,6 +44,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
                     {children}
                     <MoseyBankFooter />
                 </div>
+                <OptimizelyOneProvider value={{ debug: true }}>
+                    <PageActivator />
+                    { children }
+                    <OptimizelyOneGadget />
+                </OptimizelyOneProvider>
+                <Scripts.Footer />
             </Body>
         </ThemeProvider>
     </html>
